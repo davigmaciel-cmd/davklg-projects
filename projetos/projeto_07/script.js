52943c7d2bd061aa8f30b9239b861f1947c1ae06
DAVI GOMES MACIEL <davi_g_maciel@estudante.sesisenai.org.br>
2026-09-08 15:23:56 -0300

    teste

---
+++ projetos/projeto_07/script.js
@@ -0,0 +1,61 @@
+const cards1 = document.querySelector(".cards1");
+
+const cards2 = document.querySelector(".cards2");
+
+const cards3 = document.querySelector(".cards3");
+
+const cards4 = document.querySelector(".cards4");
+
+const cards5 = document.querySelector(".cards5");
+
+const cards6 = document.querySelector(".cards6");
+
+const cards7 = document.querySelector(".cards7");
+
+const cards8 = document.querySelector(".cards8");
+
+const cards9 = document.querySelector(".cards9");
+
+cards1.addEventListener("click", function(){
+    cards1.classList.toggle("active")
+
+})
+cards2.addEventListener("click", function(){
+    cards2.classList.toggle("active")
+
+})
+
+cards3.addEventListener("click", function(){
+    cards3.classList.toggle("active")
+
+})
+
+cards4.addEventListener("click", function(){
+    cards4.classList.toggle("active")
+
+})
+
+cards5.addEventListener("click", function(){
+    cards5.classList.toggle("active")
+
+})
+
+cards6.addEventListener("click", function(){
+    cards6.classList.toggle("active")
+
+})
+
+cards7.addEventListener("click", function(){
+    cards7.classList.toggle("active")
+
+})
+
+cards8.addEventListener("click", function(){
+    cards8.classList.toggle("active")
+
+})
+
+cards9.addEventListener("click", function(){
+    cards9.classList.toggle("active")
+
+})
